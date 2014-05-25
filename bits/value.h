a98e4342dc2382ce58eed2614e7584bd5b768042
krakjoe <joe.watkins@live.co.uk>
2014-05-25 21:26:40 +0100

    ...

--- bits/value.h
+++ bits/value.h
@@ -19,7 +19,8 @@
 #define HAVE_BITS_VALUE_H
 
 #define PHP_JIT_VALUE_FUNCTIONS \
-	JIT_FE(jit_value_get_param)
+	JIT_FE(jit_value_get_param) \
+	JIT_FE(jit_value_create_long_constant)
 
 static const char *le_jit_value_name = "jit value";
 static       int   le_jit_value;
@@ -36,7 +37,14 @@ ZEND_BEGIN_ARG_INFO_EX(jit_value_get_param_arginfo, 0, 0, 2)
 	ZEND_ARG_INFO(0, param)
 ZEND_END_ARG_INFO()
 
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_create_long_constant_arginfo, 0, 0, 2)
+	ZEND_ARG_INFO(0, function)
+	ZEND_ARG_INFO(0, constant)
+ZEND_END_ARG_INFO()
+
 PHP_FUNCTION(jit_value_get_param);
+PHP_FUNCTION(jit_value_create_long_constant);
 #else
 #ifndef HAVE_BITS_VALUE
 #define HAVE_BITS_VALUE
@@ -60,6 +68,24 @@ PHP_FUNCTION(jit_value_get_param) {
 	ZEND_FETCH_RESOURCE(function, jit_function_t, &resource, -1, le_jit_function_name, le_jit_function);
 
 	value = jit_value_get_param(function, param);
+
+	ZEND_REGISTER_RESOURCE(return_value, value, le_jit_value);
+} /* }}} */
+
+/* {{{ jit_value_t jit_value_create_long_constant(jit_function_t function, jit_type_t type, long num) */
+PHP_FUNCTION(jit_value_create_long_constant) {
+	zval *zfunction;
+	long num, type;
+	jit_function_t function;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rll", &zfunction, &type, &num) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(function, jit_function_t, &zfunction, -1, le_jit_function_name, le_jit_function);
+
+	value = jit_value_create_long_constant(function, (jit_type_t) type, num);
 
 	ZEND_REGISTER_RESOURCE(return_value, value, le_jit_value);
 } /* }}} */
---
+++ fib.php
@@ -0,0 +1,62 @@
+<?php
+/**
+* This example is based loosely on tutorial from libjit source
+*
+* http://git.savannah.gnu.org/cgit/libjit.git/tree/tutorial/t2.c
+*/
+$context = jit_context_create();
+
+jit_context_build_start($context);
+
+$signature = jit_type_create_signature(
+	JIT_ABI_CDECL, JIT_TYPE_INT, [
+	JIT_TYPE_INT
+], 1);
+
+$function = jit_function_create($context, $signature);
+
+var_dump($function, $context, $signature);
+
+$n = jit_value_get_param($function, 0);
+
+$zero  = jit_value_create_long_constant($function, JIT_TYPE_INT, 0);
+$one   = jit_value_create_long_constant($function, JIT_TYPE_INT, 1);
+$two   = jit_value_create_long_constant($function, JIT_TYPE_INT, 2);
+
+$temp1 = jit_insn_eq($function, $n, $zero);
+$label1 = jit_insn_branch_if_not($function, $temp1);
+jit_insn_return($function, $zero);
+jit_insn_label($function, $label1);
+
+$temp2 = jit_insn_eq($function, $n, $one);
+$label2 = jit_insn_branch_if_not($function, $temp2);
+jit_insn_return($function, $one);
+jit_insn_label($function, $label2);
+
+jit_insn_return($function, jit_insn_add($function, jit_insn_call($function, "Fibonacci", $function, $signature, [
+	jit_insn_sub($function, $n, $one)	
+]), jit_insn_call($function, "Fibonacci", $function, $signature, [
+	jit_insn_sub($function, $n, $two)
+])));
+
+jit_function_compile($function);
+jit_context_build_end($context);
+
+$start = microtime(true);
+var_dump(jit_function_apply($function, [40], JIT_TYPE_INT));
+printf("jit: %.3f seconds\n", microtime(true)-$start);
+
+function Fibonacci($n)
+{
+   if ( $n == 0 )
+      return 0;
+   else if ( $n == 1 )
+      return 1;
+   else
+      return ( Fibonacci($n-1) + Fibonacci($n-2) );
+}
+
+$start = microtime(true);
+var_dump(Fibonacci(40));
+printf("php: %.3f seconds\n", microtime(true)-$start);
+?>
