1f9100ef64a64706d348cf4503d534bd3625c395
krakjoe <joe.watkins@live.co.uk>
2014-05-25 18:43:13 +0100

    ...

--- jit.php
+++ jit.php
@@ -7,6 +7,8 @@ $signature = jit_type_create_signature(
 	JIT_ABI_CDECL, JIT_TYPE_INT, [
 	JIT_TYPE_INT,
 	JIT_TYPE_INT,
+	JIT_TYPE_INT, 
+	JIT_TYPE_INT,
 	JIT_TYPE_INT
 ], 1);
 
@@ -17,30 +19,34 @@ var_dump($function, $context, $signature);
 $x = jit_value_get_param($function, 0);
 $y = jit_value_get_param($function, 1);
 $z = jit_value_get_param($function, 2);
+$r = jit_value_get_param($function, 3);
+$t = jit_value_get_param($function, 4);
 
-var_dump($x, $y, $z);
+var_dump($x, $y, $z, $r, $t);
 
 $temp1 = jit_insn_mul($function, $x, $y);
 $temp2 = jit_insn_add($function, $temp1, $z);
+$temp3 = jit_insn_sub($function, $temp2, $r);
+$temp4 = jit_insn_div($function, $temp3, $t);
 
-jit_insn_return($function, $temp2);
+jit_insn_return($function, $temp4);
 
 jit_function_compile($function);
 jit_context_build_end($context);
 
 $start = microtime(true);
 for ($i = 0; $i<100000; $i++) {
-	jit_function_apply($function, [2, 8, 8], JIT_TYPE_INT);
+	jit_function_apply($function, [200, 800, 800, 500, 200], JIT_TYPE_INT);
 }
 printf("jit: %.3f seconds\n", microtime(true) - $start);
 
-function mul_add($x, $y, $z) {
-	return $x * $y + $z;
+function mul_add_sub($x, $y, $z, $r, $t) {
+	return $x * $y + $z - $r / $t;
 }
 
 $start = microtime(true);
 for ($i = 0; $i<100000; $i++) {
-	mul_add(2, 8, 8);
+	mul_add_sub(200, 800, 800, 500, 200);
 }
 printf("php: %.3f seconds\n", microtime(true) - $start);
 ?>
