1cadb7396d9a911242b98a55f9c9c68de8f505ab
krakjoe <joe.watkins@live.co.uk>
2014-05-26 08:05:00 +0100

    moar

--- bits/value.h
+++ bits/value.h
@@ -20,7 +20,15 @@
 
 #define PHP_JIT_VALUE_FUNCTIONS \
 	JIT_FE(jit_value_get_param) \
-	JIT_FE(jit_value_create_long_constant)
+	JIT_FE(jit_value_create_long_constant) \
+	JIT_FE(jit_value_create_nint_constant) \
+	JIT_FE(jit_value_create_float32_constant) \
+	JIT_FE(jit_value_create_float64_constant) \
+	JIT_FE(jit_value_create_nfloat_constant) \
+	JIT_FE(jit_value_is_temporary) \
+	JIT_FE(jit_value_is_local) \
+	JIT_FE(jit_value_is_constant) \
+	JIT_FE(jit_value_is_parameter)
 
 static const char *le_jit_value_name = "jit value";
 static       int   le_jit_value;
@@ -37,14 +45,57 @@ ZEND_BEGIN_ARG_INFO_EX(jit_value_get_param_arginfo, 0, 0, 2)
 	ZEND_ARG_INFO(0, param)
 ZEND_END_ARG_INFO()
 
-
 ZEND_BEGIN_ARG_INFO_EX(jit_value_create_long_constant_arginfo, 0, 0, 2)
 	ZEND_ARG_INFO(0, function)
-	ZEND_ARG_INFO(0, constant)
+	ZEND_ARG_INFO(0, long)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_create_nint_constant_arginfo, 0, 0, 2)
+	ZEND_ARG_INFO(0, function)
+	ZEND_ARG_INFO(0, nint)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_create_float32_constant_arginfo, 0, 0, 2)
+	ZEND_ARG_INFO(0, function)
+	ZEND_ARG_INFO(0, float32)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_create_float64_constant_arginfo, 0, 0, 2)
+	ZEND_ARG_INFO(0, function)
+	ZEND_ARG_INFO(0, float64)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_create_nfloat_constant_arginfo, 0, 0, 2)
+	ZEND_ARG_INFO(0, function)
+	ZEND_ARG_INFO(0, nfloat)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_is_temporary_arginfo, 0, 0, 1)
+	ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_is_local_arginfo, 0, 0, 1)
+	ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_is_constant_arginfo, 0, 0, 1)
+	ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(jit_value_is_parameter_arginfo, 0, 0, 1)
+	ZEND_ARG_INFO(0, value)
 ZEND_END_ARG_INFO()
 
 PHP_FUNCTION(jit_value_get_param);
 PHP_FUNCTION(jit_value_create_long_constant);
+PHP_FUNCTION(jit_value_create_nint_constant);
+PHP_FUNCTION(jit_value_create_float32_constant);
+PHP_FUNCTION(jit_value_create_float64_constant);
+PHP_FUNCTION(jit_value_create_nfloat_constant);
+PHP_FUNCTION(jit_value_is_temporary);
+PHP_FUNCTION(jit_value_is_local);
+PHP_FUNCTION(jit_value_is_constant);
+PHP_FUNCTION(jit_value_is_parameter);
 #else
 #ifndef HAVE_BITS_VALUE
 #define HAVE_BITS_VALUE
@@ -88,6 +139,134 @@ PHP_FUNCTION(jit_value_create_long_constant) {
 	value = jit_value_create_long_constant(function, (jit_type_t) type, num);
 
 	ZEND_REGISTER_RESOURCE(return_value, value, le_jit_value);
+} /* }}} */
+
+/* {{{ jit_value_t jit_value_create_nint_constant(jit_function_t function, jit_type_t type, long num) */
+PHP_FUNCTION(jit_value_create_nint_constant) {
+	zval *zfunction;
+	long num, type;
+	jit_function_t function;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rll", &zfunction, &type, &num) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(function, jit_function_t, &zfunction, -1, le_jit_function_name, le_jit_function);
+
+	value = jit_value_create_nint_constant(function, (jit_type_t) type, num);
+
+	ZEND_REGISTER_RESOURCE(return_value, value, le_jit_value);
+} /* }}} */
+
+/* {{{ jit_value_t jit_value_create_float32_constant(jit_function_t function, jit_type_t type, float float32) */
+PHP_FUNCTION(jit_value_create_float32_constant) {
+	zval *zfunction;
+	long num, type;
+	jit_function_t function;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rll", &zfunction, &type, &num) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(function, jit_function_t, &zfunction, -1, le_jit_function_name, le_jit_function);
+
+	value = jit_value_create_float32_constant(function, (jit_type_t) type, num);
+
+	ZEND_REGISTER_RESOURCE(return_value, value, le_jit_value);
+} /* }}} */
+
+/* {{{ jit_value_t jit_value_create_float64_constant(jit_function_t function, jit_type_t type, float float64) */
+PHP_FUNCTION(jit_value_create_float64_constant) {
+	zval *zfunction;
+	long num, type;
+	jit_function_t function;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rll", &zfunction, &type, &num) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(function, jit_function_t, &zfunction, -1, le_jit_function_name, le_jit_function);
+
+	value = jit_value_create_float64_constant(function, (jit_type_t) type, num);
+
+	ZEND_REGISTER_RESOURCE(return_value, value, le_jit_value);
+} /* }}} */
+
+/* {{{ jit_value_t jit_value_create_nfloat_constant(jit_function_t function, jit_type_t type, float nfloat) */
+PHP_FUNCTION(jit_value_create_nfloat_constant) {
+	zval *zfunction;
+	long num, type;
+	jit_function_t function;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rll", &zfunction, &type, &num) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(function, jit_function_t, &zfunction, -1, le_jit_function_name, le_jit_function);
+
+	value = jit_value_create_nfloat_constant(function, (jit_type_t) type, num);
+
+	ZEND_REGISTER_RESOURCE(return_value, value, le_jit_value);
+} /* }}} */
+
+/* {{{ bool jit_value_is_temporary(jit_value_t value) */
+PHP_FUNCTION(jit_value_is_temporary) {
+	zval *zvalue;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zvalue) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(value,    jit_value_t,    &zvalue,    -1, le_jit_value_name,    le_jit_value);
+	
+	RETURN_BOOL(jit_value_is_temporary(value));
+} /* }}} */
+
+/* {{{ bool jit_value_is_local(jit_value_t value) */
+PHP_FUNCTION(jit_value_is_local) {
+	zval *zvalue;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zvalue) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(value,    jit_value_t,    &zvalue,    -1, le_jit_value_name,    le_jit_value);
+	
+	RETURN_BOOL(jit_value_is_local(value));
+} /* }}} */
+
+/* {{{ bool jit_value_is_constant(jit_value_t value) */
+PHP_FUNCTION(jit_value_is_constant) {
+	zval *zvalue;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zvalue) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(value,    jit_value_t,    &zvalue,    -1, le_jit_value_name,    le_jit_value);
+	
+	RETURN_BOOL(jit_value_is_constant(value));
+} /* }}} */
+
+/* {{{ bool jit_value_is_parameter(jit_value_t value) */
+PHP_FUNCTION(jit_value_is_parameter) {
+	zval *zvalue;
+	jit_value_t value;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zvalue) != SUCCESS) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(value,    jit_value_t,    &zvalue,    -1, le_jit_value_name,    le_jit_value);
+	
+	RETURN_BOOL(jit_value_is_parameter(value));
 } /* }}} */
 #endif
 #endif
