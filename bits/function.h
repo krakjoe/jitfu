3449c4fa0d7bb935832650d0dc52b2725ef2a4aa
krakjoe <joe.watkins@live.co.uk>
2014-06-04 17:12:03 +0100

    ...

--- bits/function.h
+++ bits/function.h
@@ -392,7 +392,6 @@ PHP_METHOD(Func, __invoke) {
 				} break;
 				
 				case IS_STRING: {
-					printf("string in %s\n", Z_STRVAL_P(args[narg]));
 					jargs[narg] = &Z_STRVAL_P(args[narg]);
 				} break;
 				
@@ -402,7 +401,6 @@ PHP_METHOD(Func, __invoke) {
 				} break;
 				
 				default: {
-					printf("whatevs\n");
 					/* throw unsupport argument type from zend */
 				}
 			}
