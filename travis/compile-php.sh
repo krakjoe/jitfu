0b7e039d3ac23b608338ac41ebb0bf6f9a52fddf
krakjoe <joe.watkins@live.co.uk>
2014-08-30 17:10:12 +0100

    wrong version of php

--- travis/compile-php.sh
+++ travis/compile-php.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
-
 set -e
-
 mkdir -p $HOME/php
 git clone --depth 1 https://github.com/php/php-src $HOME/php/src
+git checkout PHP-5.6
 cd $HOME/php/src
 ./buildconf --force
 ./configure --prefix=$HOME --disable-all --enable-debug
