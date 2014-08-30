562d7a2e7554b24f101600a25c390b6a4b402edf
krakjoe <joe.watkins@live.co.uk>
2014-08-30 17:17:19 +0100

    I suck at git

--- travis/compile-php.sh
+++ travis/compile-php.sh
@@ -2,8 +2,8 @@
 set -e
 mkdir -p $HOME/php
 git clone --depth 1 https://github.com/php/php-src $HOME/php/src
-git checkout origin/PHP-5.6
 cd $HOME/php/src
+git checkout PHP-5.6
 ./buildconf --force
 ./configure --prefix=$HOME --disable-all --enable-debug
 make -j2 --quiet install
