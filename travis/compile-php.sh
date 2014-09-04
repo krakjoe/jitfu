c51b31b639448c635532c3f96c27af657acae89c
krakjoe <joe.watkins@live.co.uk>
2014-09-04 08:56:29 +0100

    enable some extenions for composer

--- travis/compile-php.sh
+++ travis/compile-php.sh
@@ -5,7 +5,18 @@ git clone https://github.com/php/php-src $HOME/php/src
 cd $HOME/php/src
 git checkout PHP-5.6
 ./buildconf --force
-./configure --prefix=$HOME --disable-all --enable-debug --with-config-file-path=$HOME --with-config-file-scan-dir=$HOME/php.d
+./configure \
+    --prefix=$HOME \
+    --disable-all \
+    --enable-debug \
+    --with-config-file-path=$HOME \
+    --with-config-file-scan-dir=$HOME/php.d \
+    --enable-json \
+    --enable-phar \
+    --enable-filter \
+    --enable-hash \
+    --enable-ctype \
+    --with-openssl
 make -j2 --quiet install
 cp php.ini-development $HOME
 mkdir $HOME/php.d
